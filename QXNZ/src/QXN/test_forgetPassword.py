9bad20cbb35d085e544cd1a34b5b0bee0b1d98d9
zhaoweibo@longruan.com <609697140@qq.com>
2018-09-10 16:54:12 +0800

    20180910 update

--- QXNZ/src/QXN/test_forgetPassword.py
+++ QXNZ/src/QXN/test_forgetPassword.py
@@ -29,25 +29,25 @@ class Forget(unittest.TestCase):
         driver.implicitly_wait(30)
         driver.maximize_window()
         driver.find_element_by_xpath("//*[@id='fm11']/div[4]/a[2]").click()
-        time.sleep(2)
+        driver.implicitly_wait(30)
         #查询企业
         driver.find_element_by_xpath("//*[@id='scjydwjbxx']/a[2]/div/div[1]/input").send_keys("测试")
-        time.sleep(2)
+        driver.implicitly_wait(30)
         driver.find_element_by_xpath("//*[@id='scjydwjbxx']/a[2]/div/div[1]/ul/li[1]").click()
-        time.sleep(2)
+        driver.implicitly_wait(30)
         driver.find_element_by_xpath("//*[@id='startFindPass']").click()
-        time.sleep(2)
+        driver.implicitly_wait(30)
         #找回密码
         driver.find_element_by_xpath("//*[@id='userTable']/tbody/tr/td[4]/button").click()
         driver.find_element_by_xpath("//*[@id='findPassForm']/div[1]/input").send_keys("mkqycs1")
         driver.find_element_by_xpath("//*[@id='findPassForm']/div[2]/input").send_keys("13000000000")
         driver.find_element_by_xpath("//*[@id='findPassForm']/div[3]/input").send_keys("123@qq.com")
         driver.find_element_by_xpath("//*[@id='findPassword']").click()
-        time.sleep(2)
+        driver.implicitly_wait(30)
         password = self.driver.find_element_by_xpath("//*[@id='layui-layer3']/div[2]").text
         self.assertEqual("找回成功：123456", password)          
         driver.find_element_by_xpath("//*[@id='layui-layer3']/div[3]/a").click()
-        time.sleep(2)
+        driver.implicitly_wait(30)
         driver.close()
         
     def tearDown(self):
