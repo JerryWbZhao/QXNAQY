9e53d37b7a402d685887a2cf8bdf189d750d298d
zhaoweibo@longruan.com <609697140@qq.com>
2018-09-20 17:04:37 +0800

    20180920

--- QXNZ/src/ZHZC/test_ZHZC1.py
+++ QXNZ/src/ZHZC/test_ZHZC1.py
@@ -52,6 +52,7 @@ class ZHZC1(unittest.TestCase):
         driver.find_element_by_xpath("//*[@id='shortName']").send_keys("生产")
         driver.find_element_by_xpath("//*[@id='contact']").send_keys("单位法人")
         driver.find_element_by_xpath("//*[@id='telephone']").send_keys("13000000000")
+        
         sel1 = driver.find_element_by_xpath("//*[@id='qyxzqh1']")
         Select(sel1).select_by_value('522300000000')
         sel2 = driver.find_element_by_xpath("//*[@id='qyxzqh2']")
